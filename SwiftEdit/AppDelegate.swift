6017b57fa4d4d4400baae81e89695edf8576f6cd
Scott Horn <scott.horn@wotifgroup.com>
2014-06-14 14:36:02 +1000

    Initial Commit

---
+++ SwiftEdit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftEdit
+//
+//  Created by Scott Horn on 14/06/2014.
+//  Copyright (c) 2014 Scott Horn. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SwiftEdit/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  SwiftEdit
+//
+//  Created by Scott Horn on 14/06/2014.
+//  Copyright (c) 2014 Scott Horn. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
