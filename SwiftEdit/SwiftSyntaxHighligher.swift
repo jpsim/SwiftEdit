31baebf948fd869925c31ef331d19a3ee20ca49b
Brandon Evans <brandon@brandonevans.ca>
2017-11-27 19:45:14 -0700

    Replace shelling out with SourceKittenFramework

--- SwiftEdit/SwiftSyntaxHighligher.swift
+++ SwiftEdit/SwiftSyntaxHighligher.swift
@@ -7,6 +7,7 @@
 //
 
 import Cocoa
+import SourceKittenFramework
 
 extension NSAttributedStringKey {
     static let swiftElementType = NSAttributedStringKey(rawValue: "swiftElementType")
@@ -17,64 +18,25 @@ struct Token {
     let range: NSRange
 }
 
-public enum SyntaxKind: String {
-    /// `argument`.
-    case Argument = "source.lang.swift.syntaxtype.argument"
-    /// `attribute.builtin`.
-    case AttributeBuiltin = "source.lang.swift.syntaxtype.attribute.builtin"
-    /// `attribute.id`.
-    case AttributeID = "source.lang.swift.syntaxtype.attribute.id"
-    /// `buildconfig.id`.
-    case BuildconfigID = "source.lang.swift.syntaxtype.buildconfig.id"
-    /// `buildconfig.keyword`.
-    case BuildconfigKeyword = "source.lang.swift.syntaxtype.buildconfig.keyword"
-    /// `comment`.
-    case Comment = "source.lang.swift.syntaxtype.comment"
-    /// `comment.mark`.
-    case CommentMark = "source.lang.swift.syntaxtype.comment.mark"
-    /// `comment.url`.
-    case CommentURL = "source.lang.swift.syntaxtype.comment.url"
-    /// `doccomment`.
-    case DocComment = "source.lang.swift.syntaxtype.doccomment"
-    /// `doccomment.field`.
-    case DocCommentField = "source.lang.swift.syntaxtype.doccomment.field"
-    /// `identifier`.
-    case Identifier = "source.lang.swift.syntaxtype.identifier"
-    /// `keyword`.
-    case Keyword = "source.lang.swift.syntaxtype.keyword"
-    /// `number`.
-    case Number = "source.lang.swift.syntaxtype.number"
-    /// `objectliteral`.
-    case Objectliteral = "source.lang.swift.syntaxtype.objectliteral"
-    /// `parameter`.
-    case Parameter = "source.lang.swift.syntaxtype.parameter"
-    /// `placeholder`.
-    case Placeholder = "source.lang.swift.syntaxtype.placeholder"
-    /// `string`.
-    case String = "source.lang.swift.syntaxtype.string"
-    /// `string_interpolation_anchor`.
-    case StringInterpolationAnchor = "source.lang.swift.syntaxtype.string_interpolation_anchor"
-    /// `typeidentifier`.
-    case Typeidentifier = "source.lang.swift.syntaxtype.typeidentifier"
-
+extension SyntaxKind {
     var colorValue: NSColor {
         switch self {
-        case .Keyword:
+        case .keyword:
             return NSColor(red: 0.796, green: 0.208, blue: 0.624, alpha: 1)
-        case .Identifier:
-            return NSColor.black
-        case .Typeidentifier:
+        case .identifier:
+            return .black
+        case .typeidentifier:
             return NSColor(red: 0.478, green: 0.251, blue: 0.651, alpha: 1)
-        case .String:
+        case .string:
             return NSColor(red: 0.918, green: 0.216, blue: 0.071, alpha: 1)
-        case .Number:
+        case .number:
             return NSColor(red: 0.22, green: 0.18, blue: 0.827, alpha: 1)
-        case .Comment, .CommentMark, .CommentURL, .DocComment, .DocCommentField:
+        case .comment, .commentMark, .commentURL, .docComment, .docCommentField:
             return NSColor(red: 0, green: 0.514, blue: 0.122, alpha: 1)
-        case .AttributeBuiltin:
+        case .attributeBuiltin:
             return NSColor(red: 0.796, green: 0.208, blue: 0.624, alpha: 1)
         default:
-            return NSColor.green
+            return .green
         }
     }
 }
@@ -118,40 +80,24 @@ class SwiftSyntaxHighligher: NSObject, NSTextStorageDelegate, NSLayoutManagerDel
         for layoutManager in layoutManagerList {
             layoutManager.delegate = self
             layoutManager.removeTemporaryAttribute(.swiftElementType,
-                forCharacterRange: range)
+                                                   forCharacterRange: range)
 
             for token in tokens {
                 layoutManager.addTemporaryAttributes([.swiftElementType: token.kind],
-                    forCharacterRange: token.range)
+                                                     forCharacterRange: token.range)
             }
         }
     }
 
     func parseString(string: String) -> [Token]? {
-        // Shell out to SourceKitten to obtain syntax map for string
-        if(string.isEmpty){
+        if string.isEmpty {
             return []
         }
-        
-        let syntaxPipe = Pipe()
 
-        let syntaxTask = Process()
-        syntaxTask.launchPath = "/usr/local/bin/sourcekitten"
-        syntaxTask.arguments = ["syntax", "--text", string]
-        syntaxTask.standardOutput = syntaxPipe
+        let syntaxMap = SyntaxMap(file: File(contents: string))
 
-        syntaxTask.launch()
-        syntaxTask.waitUntilExit()
-
-        let syntaxMap = NSMutableString(data: syntaxPipe.fileHandleForReading.readDataToEndOfFile(), encoding: String.Encoding.utf8.rawValue)
-
-        let tokens = try! JSONSerialization.jsonObject(with: syntaxMap!.data(using: String.Encoding.utf8.rawValue)!,
-            options: []) as! [NSDictionary]
-        return tokens.map { token in
-            let offset = token["offset"] as! Int
-            let length = token["length"] as! Int
-            let type = token["type"] as! String
-            return Token(kind: type, range: NSRange(location: offset, length: length))
+        return syntaxMap.tokens.map { token in
+            return Token(kind: token.type, range: NSRange(location: token.offset, length: token.length))
         }
     }
 
